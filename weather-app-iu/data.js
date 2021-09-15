99beadae26855cb00cb0b3a237980131d89388e3
touraye <touraye07@gmail.com>
2021-09-15 12:42:10 +0000

    added weather app

---
+++ weather-app-iu/data.js
@@ -0,0 +1,523 @@
+const data = [
+    {
+        img: './assets/bjl.jpg',
+        city: 'banjul',
+        temperature: 12,
+        weather_con: 'couldy',
+        humidity: 64,
+        wind: 12,
+        days: [
+            {                 
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 17,
+                detail: 'sunny'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 13,
+                detail: 'partly could'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 6,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        img: './assets/dakar.jpg',
+        city: 'dakar',
+        temperature: 13,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'lagos',
+        temperature: 13,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'bamako',
+        temperature: 17,
+        weather_con: 'partly couldy',
+        humidity: 34,
+        wind: 5,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'accra',
+        temperature: 14,
+        weather_con: 'couldy',
+        humidity: 52,
+        wind: 10,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'conakerey',
+        temperature: 13,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'cape town',
+        temperature: 13,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'kampala',
+        temperature: 11,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,      
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+    {
+        city: 'casablanca',
+        temperature: 22,
+        weather_con: 'partly couldy',
+        humidity: 54,
+        wind: 10,  
+        days: [
+            {    
+                day: 'mon'        ,
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'tue',
+                icon: 'assets',
+                tem: 9,
+                detail: 'raining'
+            },
+             {
+                 day: 'wed',
+                icon: 'assets',
+                tem: 15,
+                detail: 'sunny'
+            },
+             {
+                 day: 'thur',
+                icon: 'assets',
+                tem: 11,
+                detail: 'couldy'
+            },
+            {
+                day: 'fri',
+                icon: 'assets',
+                tem: 7,
+                detail: 'storm'
+            },
+            {
+                day: 'sat',
+                icon: 'assets',
+                tem: 18,
+                detail: 'sunny'
+            },          
+             {
+                day: 'sun',
+                icon: 'assets',
+                tem: 10,
+                detail: 'storm'
+            }
+        ]
+    },
+]
+
+
+// 
+const card = document.querySelector('.card');
+const previewImg = document.querySelector('.preview');
+const city_name = document.querySelector('.city');
+const temperature = document.querySelector('.temrature');
+const weather_con = document.querySelector('.weather');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+const day_tem = document.querySelectorAll('.day-tem');
+const day_condition = document.querySelectorAll('.day-condition');
+const btn = document.getElementById('weatherBtn');
+
+
+
+let counter = 0;
+// Load event
+window.addEventListener('DOMContentLoaded', () => {
+    // newData(data);
+    displayWeather(counter)
+})
+
+
+// loop through the data
+// const newData = (data) => {
+//     const list = data.flatMap(item => {        
+//         console.table(`the temperature in ${item.city} is : ${item.temperature}, the weather condition is ${weather_con.textContent}, humidity is ${item.humidity}, wind pressure is ${item.wind}`);
+//         let dailyWeather = item.days;
+//         dailyWeather.map(day => {
+//             // day.slice(0,4);
+//             console.log(`Day : ${day.day} tem : ${day.tem} detail : ${day.detail}`);
+//         })
+//     })
+//     return list;
+// }
+
+// collection data from the array
+const displayWeather = (list) => {
+    let cityWeather = data[list];    
+    
+    previewImg.src = cityWeather.img
+    city_name.textContent = cityWeather.city;
+    temperature.textContent = cityWeather.temperature;
+    weather_con.textContent = cityWeather.weather_con;
+    humidity.textContent = cityWeather.humidity;
+    wind.textContent = cityWeather.wind;
+    // collect days
+    day_tem.textContent = cityWeather.days[2]
+    day_condition.textContent = cityWeather.days[3]
+}
+
+// Weather Random Button
+btn.addEventListener('click', () =>{
+    counter++;
+    if(counter > data.length - 1){
+        counter = 0;
+    }
+    displayWeather(counter)
+})
